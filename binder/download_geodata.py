f9dc3625371d25667dbad50f2f413ab4e8d0e428
chekos <chekos.data@gmail.com>
2019-04-18 17:01:50 -0700

    Update download_geodata.py

--- binder/download_geodata.py
+++ binder/download_geodata.py
@@ -3,7 +3,7 @@ import requests
 import io
 from zipfile import ZipFile
 
-RAW_DATA_PATH = Path("exercises/data/raw/")
+RAW_DATA_PATH = Path("exercises/data/raw/counties/")
 
 url = "https://www2.census.gov/geo/tiger/TIGER2018/COUNTY/tl_2018_us_county.zip"
 site = requests.get(url)
